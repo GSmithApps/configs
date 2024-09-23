389d6c4465d5c3caedd02c3884bace6f07525f76
GSmithApps <14.gsmith.14@gmail.com>
2024-09-22 23:13:33 -0500

    my virtual machine

--- nix/my-virtual-machine-configuration.nix
+++ nix/my-virtual-machine-configuration.nix
@@ -97,8 +97,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        jnoortheen.nix-ide
+      ];
+    })
   ];
   
   fileSystems."/mnt/shared" = {
