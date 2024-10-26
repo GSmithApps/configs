5a37654eaddc80564b29b15c448d667a2b1b29c0
GSmithApps <14.gsmith.14@gmail.com>
2024-10-25 21:46:37 -0500

    added flake and other files for nix laptop

---
+++ nix-laptop/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "NixOS configuration";
+  
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./config.nix
+      ];
+    };
+  
+  };
+}
